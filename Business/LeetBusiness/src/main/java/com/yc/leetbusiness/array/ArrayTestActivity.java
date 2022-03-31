6b58bf4abd3618b4dbcd530bb48daa0f549346d9
yangchong <yangchong211@163.com>
2022-03-31 11:30:45 +0800

    刷算法，调整数组顺序

--- Business/LeetBusiness/src/main/java/com/yc/leetbusiness/array/ArrayTestActivity.java
+++ Business/LeetBusiness/src/main/java/com/yc/leetbusiness/array/ArrayTestActivity.java
@@ -191,6 +191,36 @@ public class ArrayTestActivity extends BaseActivity implements View.OnClickListe
         return count;
     }
 
+    /**
+     * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，所有的偶数位于位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+     */
+    public class Solution {
+        public void reOrderArray(int [] array) {
+            //如果数组长度等于0或者等于1，什么都不做直接返回
+            if(array.length==0||array.length==1)
+                return;
+            //oddCount：保存奇数个数
+            //oddBegin：奇数从数组头部开始添加
+            int oddCount=0,oddBegin=0;
+            //新建一个数组
+            int[] newArray = new int[array.length];
+            //计算出（数组中的奇数个数）开始添加元素
+            for(int i=0;i<array.length;i++){
+                if((array[i]&1)==1) oddCount++;
+            }
+            for(int i=0;i<array.length;i++){
+                //如果数为基数新数组从头开始添加元素
+                //如果为偶数就从oddCount（数组中的奇数个数）开始添加元素
+                if((array[i]&1)==1)
+                    newArray[oddBegin++]=array[i];
+                else newArray[oddCount++]=array[i];
+            }
+            for(int i=0;i<array.length;i++){
+                array[i]=newArray[i];
+            }
+        }
+    }
+
     private class MyThread implements Runnable{
         @Override
         public void run() {
