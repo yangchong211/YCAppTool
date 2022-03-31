993c3ba7e9596bfc86b3f49b26d23922d169edfd
yangchong <yangchong211@163.com>
2022-03-31 11:29:52 +0800

    刷算法，买股票最佳时机算法刷题

--- Business/LeetBusiness/src/main/java/com/yc/leetbusiness/array/ArrayTestActivity.java
+++ Business/LeetBusiness/src/main/java/com/yc/leetbusiness/array/ArrayTestActivity.java
@@ -166,6 +166,31 @@ public class ArrayTestActivity extends BaseActivity implements View.OnClickListe
 
     }
 
+
+    /**
+     * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+     *     输入: [7,1,5,3,6,4]
+     *     输出: 7
+     *     解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得
+     *     利润 = 5-1 = 4 。
+     *     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得
+     *     利润 = 6-3 = 3 。
+     */
+    int maxProfit(int[] prices) {
+        if (prices==null || prices.length==0){
+            return 0;
+        }
+        int count = 0;
+        int temp;
+        for(int i = 1; i < prices.length; i++){
+            temp = prices[i] - prices[i-1];
+            if(temp > 0){
+                count+=temp;
+            }
+        }
+        return count;
+    }
+
     private class MyThread implements Runnable{
         @Override
         public void run() {
