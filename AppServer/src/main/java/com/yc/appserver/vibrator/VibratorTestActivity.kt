2ce2f3e5f37fe45b24bcf400539f37a8540ca9cc
yangchong <yangchong211@163.com>
2022-08-01 17:35:05 +0800

    完善笔记

--- AppServer/src/main/java/com/yc/appserver/vibrator/VibratorTestActivity.kt
+++ AppServer/src/main/java/com/yc/appserver/vibrator/VibratorTestActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.yc.appserver.R
+import com.yc.bellsvibrations.MediaAudioPlayer
 import com.yc.bellsvibrations.VibratorHelper
 import com.yc.logclient.LogUtils
 import com.yc.monitorfilelib.FileExplorerActivity
@@ -22,7 +23,11 @@ class VibratorTestActivity : AppCompatActivity() {
     private lateinit var tvTest5: RoundTextView
     private lateinit var tvTest6: RoundTextView
     private lateinit var tvTest7: RoundTextView
-    private val vibratorHelper: VibratorHelper?= null
+    private lateinit var  tvMedia1:RoundTextView
+    private lateinit var  tvMedia2:RoundTextView
+    private lateinit var  tvMedia3:RoundTextView
+    private var vibratorHelper: VibratorHelper?= null
+    private var mediaAudioPlayer: MediaAudioPlayer?= null
 
     companion object{
         fun startActivity(context: Context) {
@@ -39,6 +44,8 @@ class VibratorTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vibrator_main)
+        vibratorHelper = VibratorHelper(this)
+        mediaAudioPlayer = MediaAudioPlayer(this)
         initView()
         initListener()
         LogUtils.i("Log test info : LogTestActivity is create")
@@ -48,6 +55,9 @@ class VibratorTestActivity : AppCompatActivity() {
         tvTest1 = findViewById(R.id.tv_vibrator_1)
         tvTest2 = findViewById(R.id.tv_vibrator_2)
         tvTest3 = findViewById(R.id.tv_vibrator_3)
+        tvMedia1 = findViewById(R.id.tv_media_1)
+        tvMedia2 = findViewById(R.id.tv_media_2)
+        tvMedia3 = findViewById(R.id.tv_media_3)
     }
 
     private fun initListener() {
@@ -62,6 +72,18 @@ class VibratorTestActivity : AppCompatActivity() {
         tvTest3.setOnClickListener {
             ToastUtils.showRoundRectToast("震动3秒")
             vibratorHelper?.vibrate(3000)
+        }
+        tvMedia1.setOnClickListener {
+            ToastUtils.showRoundRectToast("开始播放raw")
+            mediaAudioPlayer?.play(MediaAudioPlayer.DataSource.DATA_SOURCE_URI,R.raw.audio_call_ring)
+        }
+        tvMedia2.setOnClickListener {
+            ToastUtils.showRoundRectToast("开始播放asset")
+            mediaAudioPlayer?.play(MediaAudioPlayer.DataSource.DATA_SOURCE_ASSET,"audio_call_ring.mp3")
+        }
+        tvMedia3.setOnClickListener {
+            ToastUtils.showRoundRectToast("取消播放")
+            mediaAudioPlayer?.stop()
         }
     }
 
---
+++ ServerLib/BellsVibrations/src/main/java/com/yc/bellsvibrations/MediaAudioPlayer.java
@@ -0,0 +1,166 @@
+package com.yc.bellsvibrations;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.net.Uri;
+
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * <pre>
+ *     @author yangchong
+ *     email  : yangchong211@163.com
+ *     time  : 2018/8/6
+ *     desc  : 音频播放player
+ *     revise:
+ * </pre>
+ */
+public final class MediaAudioPlayer {
+
+    private final Context mContext;
+    private MediaPlayer mMediaPlayer;
+    private boolean mPause = false;
+
+    public MediaAudioPlayer(Context context) {
+        this.mContext = context;
+    }
+
+    /**
+     * 播放raw资源
+     */
+    public void play(@DataSourceType int type, Object data) {
+        if (data == null) {
+            return;
+        }
+        if (type == DataSource.DATA_SOURCE_URI){
+            if (!(data instanceof Integer)){
+                throw new IllegalStateException("data must be Integer");
+            }
+        }
+        if (type == DataSource.DATA_SOURCE_ASSET){
+            if (!(data instanceof String)){
+                throw new IllegalStateException("data must be String");
+            }
+        }
+        if (type == DataSource.DATA_SOURCE_NET){
+            if (!(data instanceof String)){
+                throw new IllegalStateException("data must be String");
+            }
+        }
+        if (mMediaPlayer == null){
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mMediaPlayer.setOnCompletionListener(new OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer player) {
+                    if (mMediaPlayer != null && player != null && mMediaPlayer == player) {
+                        try {
+                            mMediaPlayer.stop();
+                            mMediaPlayer.release();
+                        } catch (IllegalStateException e) {
+                            e.printStackTrace();
+                        } finally {
+                            mMediaPlayer = null;
+                        }
+                    }
+                }
+            });
+            mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    try {
+                        mMediaPlayer.start();
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        if (mMediaPlayer != null) {
+            try {
+                switch (type) {
+                    case 1:
+                        //播放asset文件
+                        AssetFileDescriptor afd = mContext.getAssets().openFd((String) data);
+                        //AssetFileDescriptor afd = mContext.getResources().openRawResourceFd((Integer) data);
+                        mMediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                        break;
+                    case 2:
+                        //res/raw文件中的url地址。
+                        Uri sound = Uri.parse("android.resource://" + mContext.getPackageName() + "/" + data);
+                        mMediaPlayer.setDataSource(mContext, sound);
+                        break;
+                    case 3:
+                        //播放网络文件
+                        mMediaPlayer.setDataSource((String) data);
+                        break;
+                    default:
+                        break;
+                }
+                mMediaPlayer.prepare();
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 停止播放
+     */
+    public void stop() {
+        if (mMediaPlayer != null) {
+            synchronized (mMediaPlayer) {
+                try {
+                    mMediaPlayer.stop();
+                    mMediaPlayer.release();
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                } finally {
+                    mMediaPlayer = null;
+                }
+            }
+        }
+    }
+
+    public void release() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    public void pause() {
+        if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+            mMediaPlayer.pause();
+            mPause = true;
+        }
+    }
+
+    public void resume() {
+        if (mMediaPlayer != null && mPause) {
+            mMediaPlayer.start();
+            mPause = false;
+        }
+    }
+
+    public boolean isPlaying() {
+        return mMediaPlayer != null && mMediaPlayer.isPlaying();
+    }
+
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface DataSource {
+        int DATA_SOURCE_ASSET = 1;
+        int DATA_SOURCE_URI = 2;
+        int DATA_SOURCE_NET = 3;
+    }
+
+    @IntDef({DataSource.DATA_SOURCE_URI,DataSource.DATA_SOURCE_ASSET,DataSource.DATA_SOURCE_NET})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface DataSourceType{}
+}
