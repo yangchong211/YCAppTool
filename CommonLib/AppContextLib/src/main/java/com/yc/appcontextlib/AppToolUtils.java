faac03987c5ec909926a796183be7ff523581d1d
yangchong <yangchong211@163.com>
2022-11-27 08:45:01 +0800

    feat：完善工具库

--- CommonLib/AppContextLib/src/main/java/com/yc/appcontextlib/AppToolUtils.java
+++ CommonLib/AppContextLib/src/main/java/com/yc/appcontextlib/AppToolUtils.java
@@ -2,6 +2,12 @@ package com.yc.appcontextlib;
 
 import android.app.Application;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.io.Closeable;
+import java.io.IOException;
+
 /**
  * <pre>
  *     @author 杨充
@@ -30,5 +36,54 @@ public final class AppToolUtils {
             throw new NullPointerException("u should init first");
         }
     }
+
+
+    @NonNull
+    public static <T> T checkNotNull(@Nullable T arg) {
+        return checkNotNull(arg, "Argument must not be null");
+    }
+
+    @NonNull
+    public static <T> T checkNotNull(@Nullable T arg, @NonNull String message) {
+        if (arg == null) {
+            throw new NullPointerException(message);
+        }
+        return arg;
+    }
+
+
+    /**
+     * 关闭 IO
+     *
+     * @param closeables closeables
+     */
+    public static void closeIO(final Closeable... closeables) {
+        if (closeables == null) {
+            return;
+        }
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public static boolean isSpace(final String s) {
+        if (s == null) {
+            return true;
+        }
+        for (int i = 0, len = s.length(); i < len; ++i) {
+            if (!Character.isWhitespace(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
