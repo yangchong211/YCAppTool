b4da245b8bf2aef97d1707d4b2e491e07ace38ce
yangchong <yangchong211@163.com>
2023-02-01 15:36:00 +0800

    feat：哈希算法运用

--- CommonLib/AppEncryptLib/src/main/java/com/yc/appencryptlib/HashEncryptUtils.java
+++ CommonLib/AppEncryptLib/src/main/java/com/yc/appencryptlib/HashEncryptUtils.java
@@ -17,10 +17,13 @@ public final class HashEncryptUtils {
 
     /**
      * String字符串hash算法
+     * String类的hashCode，根据String类包含的字符串的内容，根据一种特殊算法返回哈希码，只要字符串的内容相同，返回的哈希码也相同。
      * @param string            字符串
      * @return
      */
     public static int hashCode(String string) {
+        Object obj = 1;
+        obj.hashCode();
         int h = hash;
         final int len = string.length();
         if (h == 0 && len > 0) {
@@ -31,7 +34,6 @@ public final class HashEncryptUtils {
         }
         return h;
     }
-
 
 
 }
