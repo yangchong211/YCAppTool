8b0c2cb204435ff0c0a4eb9bcd1868009d9d82aa
yangchong <yangchong211@163.com>
2023-06-12 12:48:10 +0800

    feat：完善文档

--- Business/ThreadBusiness/src/main/java/com/yc/ycthreadpool/App.java
+++ Business/ThreadBusiness/src/main/java/com/yc/ycthreadpool/App.java
@@ -17,44 +17,9 @@ public class App {
     }
 
     public void init() {
-        //初始化线程池管理器
-        initThreadPool();
-        ScheduleTask.getInstance().schedule(new Runnable() {
-            @Override
-            public void run() {
-                //做一些耗时任务
 
-            }
-        });
     }
 
-
-    /**
-     * 初始化线程池管理器
-     */
-    private void initThreadPool() {
-        // 创建一个独立的实例进行使用
-        executor = PoolThread.ThreadBuilder
-                .createFixed(5)
-                .setPriority(Thread.MAX_PRIORITY)
-                .setCallback(new LogCallback())
-                .build();
-    }
-
-    /**
-     * 获取线程池管理器对象，统一的管理器维护所有的线程池
-     * @return                      executor对象
-     */
-    public PoolThread getExecutor(){
-        if(executor ==null){
-            executor = PoolThread.ThreadBuilder
-                    .createFixed(5)
-                    .setPriority(Thread.MAX_PRIORITY)
-                    .setCallback(new LogCallback())
-                    .build();
-        }
-        return executor;
-    }
 
 
 
