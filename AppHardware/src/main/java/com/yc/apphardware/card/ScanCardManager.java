3cd99fd36bca69abd9c29b04014a12f4b0593f75
yangchong <yangchong211@163.com>
2024-01-16 09:09:15 +0800

    feat：完善文档

--- AppHardware/src/main/java/com/yc/apphardware/card/ScanCardManager.java
+++ AppHardware/src/main/java/com/yc/apphardware/card/ScanCardManager.java
@@ -90,107 +90,6 @@ public class ScanCardManager {
         return substring;
     }
 
-    private String joinM1Data(String cardNum, String flipCardNum, ArrayList<String> arrayList) {
-        StringBuilder sb = new StringBuilder();
-        String key_6 = arrayList.get(0);
-//        byte[] password6 = MyConverterTool.HexToByteArr(key_6);
-        byte[] password6 = BytesHexStrUtils.hexToBytes1(key_6);
-        if (!TextUtils.isEmpty(key_6) && key_6.length() == 12) {
-            int block61 = 24;
-            byte[] data61 = CardHelper.getInstance().getM1Card().readBlock(cardNum, CardReadManager.KEY_MODE_A, block61, password6);
-            String data61String = "";
-            if (data61 != null) {
-                data61String = BytesHexStrUtils.bytesToHex(data61).replace("0", "");
-            }
-            if (data61 != null && !TextUtils.isEmpty(data61String)) {
-                byte[] data62 = CardHelper.getInstance().getM1Card().readBlock(cardNum, CardReadManager.KEY_MODE_A, block61 + 1, password6);
-                byte[] data63 = CardHelper.getInstance().getM1Card().readBlock(cardNum, CardReadManager.KEY_MODE_A, block61 + 2, password6);
-                //byte[] data63 = CardReadManager.getInstance().CardM1BlockRead((byte) 26);
-                if (data62 != null && data63 != null) {
-                    String s2 = ByteUtils.bytesToHex(data62);
-                    String s3 = ByteUtils.bytesToHex(data63);
-                    sb.append(data61String).append(s2).append(s3);
-                    AppLogUtils.d("Card M1 扇区6密钥认证成功：" + sb);
-                } else {
-                    AppLogUtils.d("Card M1 扇区6密钥认证失败2：请检查秘钥-" + key_6);
-                    if (mOnScanCardListen != null) {
-                        mOnScanCardListen.onScanCard("扇区6读取失败", null);
-                    }
-                    return null;
-                }
-            } else {
-                AppLogUtils.d("Card M1 扇区6密钥认证失败1：请检查秘钥-" + key_6);
-                if (mOnScanCardListen != null) {
-                    mOnScanCardListen.onScanCard("扇区6读取失败", null);
-                }
-                return null;
-            }
-        }
-
-        String key_7 = arrayList.get(1);
-        if (!TextUtils.isEmpty(key_7) && key_7.length() == 12) {
-            int block71 = 28;
-            byte[] data71 = readBlock(cardNum, block71, key_7);
-            String data71String = "";
-            if (data71 != null) {
-                data71String = BytesHexStrUtils.bytesToHex(data71).replace("0", "");
-            }
-            if (data71 != null && !TextUtils.isEmpty(data71String)) {
-                byte[] data72 = readBlock(cardNum, block71 + 1, key_7);
-                byte[] data73 = readBlock(cardNum, block71 + 2, key_7);
-                if (data72 != null && data73 != null) {
-                    String s2 = ByteUtils.bytesToHex(data72);
-                    String s3 = ByteUtils.bytesToHex(data73);
-                    sb.append(data71String).append(s2).append(s3);
-                    AppLogUtils.d("Card M1 扇区7密钥认证成功：" + data71String + s2 + s3);
-                } else {
-                    AppLogUtils.d("Card M1 扇区7密钥认证失败2：请检查秘钥-" + key_7);
-                    if (mOnScanCardListen != null) {
-                        mOnScanCardListen.onScanCard("扇区7读取失败", null);
-                    }
-                    return null;
-                }
-            } else {
-                AppLogUtils.d("Card M1 扇区7密钥认证失败1：请检查秘钥-" + key_7);
-                if (mOnScanCardListen != null) {
-                    mOnScanCardListen.onScanCard("扇区7读取失败", null);
-                }
-                return null;
-            }
-        }
-
-        String key_8 = arrayList.get(2);
-        if (!TextUtils.isEmpty(key_8) && key_8.length() == 12) {
-            int block81 = 32;
-            byte[] data81 = readBlock(cardNum, block81, key_8);
-            String data81String = "";
-            if (data81 != null) {
-                data81String = ByteUtils.bytesToHex(data81);
-            }
-            if (data81 != null && !TextUtils.isEmpty(data81String)) {
-                byte[] data82 = readBlock(cardNum, block81 + 1, key_8);
-                if (data82 != null) {
-                    String s2 = ByteUtils.bytesToHex(data82);
-                    sb.append(data81String).append(s2);
-                    AppLogUtils.d("Card M1 扇区8密钥认证成功：" + data81String + s2);
-                } else {
-                    AppLogUtils.d("Card M1 扇区8密钥认证失败2：请检查秘钥-" + key_8);
-                    if (mOnScanCardListen != null) {
-                        mOnScanCardListen.onScanCard("扇区8读取失败", null);
-                    }
-                    return null;
-                }
-            } else {
-                AppLogUtils.d("Card M1 扇区8密钥认证失败1：请检查秘钥-" + key_8);
-                if (mOnScanCardListen != null) {
-                    mOnScanCardListen.onScanCard("扇区8读取失败", null);
-                }
-                return null;
-            }
-        }
-        return sb.toString();
-    }
-
     private byte[] readBlock(String cardNum, int block, String key) {
         if (TextUtils.isEmpty(cardNum)) {
             return null;
@@ -225,7 +124,7 @@ public class ScanCardManager {
             AppLogUtils.d("Card M1 cardKeyArray = " + MyConverterTool.ByteArrToHex(cardKeyArray) + " , taKeys " + taKeys);
             ArrayList<String> keyArray = CardHelper.getInstance().getM1Card().getCardSecretList(taKeys);
             if (keyArray != null) {
-                String cardData = joinM1Data(cardNum, flipCardNum, keyArray);
+                String cardData = joinM1Data1(cardNum, keyArray);
                 AppLogUtils.d("Card M1 cardData = " + cardData);
                 if (TextUtils.isEmpty(cardData)) {
                     return;
@@ -251,6 +150,78 @@ public class ScanCardManager {
                 mOnScanCardListen.onScanCard(byteArrayResult.getMsg(), null);
             }
         }
+    }
+
+
+    private String joinM1Data1(String flipCardNum, ArrayList<String> arrayList) {
+        StringBuilder sb = new StringBuilder();
+        String key_6 = arrayList.get(0);
+        if (!TextUtils.isEmpty(key_6) && key_6.length() == 12) {
+            byte[] password = MyConverterTool.HexToByteArr(key_6);
+            byte[] cardNo = MyConverterTool.HexToByteArr(flipCardNum);
+            boolean sector6Auth = CardReadManager.getInstance().CardM1Authority(CardReadManager.KEY_MODE_A, password, cardNo, (byte) 24);
+            Log.d("Card M1", "扇区6 " + CardReadManager.KEY_MODE_A + " , " + key_6 + " , " + flipCardNum + " , " + sector6Auth);
+            if (sector6Auth) {
+                byte[] sector6_1 = CardReadManager.getInstance().CardM1BlockRead((byte) 24);
+                byte[] sector6_2 = CardReadManager.getInstance().CardM1BlockRead((byte) 25);
+                byte[] sector6_3 = CardReadManager.getInstance().CardM1BlockRead((byte) 26);
+                String s61 = ByteUtils.bytesToHex(sector6_1);
+                String s62 = ByteUtils.bytesToHex(sector6_2);
+                String s63 = ByteUtils.bytesToHex(sector6_3);
+                AppLogUtils.d("Card M1 扇区6密钥认证成功：" + s61 + s62 + s63);
+                sb.append(s61).append(s62).append(s63);
+            } else {
+                AppLogUtils.d("M1 扇区6密钥认证失败");
+                if (mOnScanCardListen != null) {
+                    mOnScanCardListen.onScanCard("扇区读取失败", null);
+                }
+                return null;
+            }
+        }
+
+        String key_7 = arrayList.get(1);
+        if (!TextUtils.isEmpty(key_7) && key_7.length() == 12) {
+            boolean sector7Auth = CardReadManager.getInstance().CardM1Authority(CardReadManager.KEY_MODE_A, MyConverterTool.HexToByteArr(key_7), MyConverterTool.HexToByteArr(flipCardNum), (byte) 28);
+            Log.d("Card M1", "扇区7 " + CardReadManager.KEY_MODE_A + " , " + key_7 + " , " + flipCardNum + " , " + sector7Auth);
+
+            if (sector7Auth) {
+                byte[] sector7_1 = CardReadManager.getInstance().CardM1BlockRead((byte) 28);
+                byte[] sector7_2 = CardReadManager.getInstance().CardM1BlockRead((byte) 29);
+                byte[] sector7_3 = CardReadManager.getInstance().CardM1BlockRead((byte) 30);
+                String s71 = ByteUtils.bytesToHex(sector7_1);
+                String s72 = ByteUtils.bytesToHex(sector7_2);
+                String s73 = ByteUtils.bytesToHex(sector7_3);
+                AppLogUtils.d("Card M1 扇区7密钥认证成功：" + s71 + s72 + s73);
+                sb.append(s71).append(s72).append(s73);
+            } else {
+                AppLogUtils.d("M1 扇区7密钥认证失败");
+                if (mOnScanCardListen != null) {
+                    mOnScanCardListen.onScanCard("扇区读取失败", null);
+                }
+                return null;
+            }
+        }
+
+        String key_8 = arrayList.get(2);
+        if (!TextUtils.isEmpty(key_8) && key_8.length() == 12) {
+            boolean sector8Auth = CardReadManager.getInstance().CardM1Authority(CardReadManager.KEY_MODE_A, MyConverterTool.HexToByteArr(key_8), MyConverterTool.HexToByteArr(flipCardNum), (byte) 32);
+            Log.d("Card M1", "扇区8 " + CardReadManager.KEY_MODE_A + " , " + key_8 + " , " + flipCardNum + " , " + sector8Auth);
+            if (sector8Auth) {
+                byte[] sector8_1 = CardReadManager.getInstance().CardM1BlockRead((byte) 32);
+                byte[] sector8_2 = CardReadManager.getInstance().CardM1BlockRead((byte) 33);
+                String s81 = ByteUtils.bytesToHex(sector8_1);
+                String s82 = ByteUtils.bytesToHex(sector8_2);
+                AppLogUtils.d("Card M1 扇区8密钥认证成功：" + s81 + s82);
+                sb.append(ByteUtils.bytesToHex(sector8_1)).append(ByteUtils.bytesToHex(sector8_2));
+            } else {
+                AppLogUtils.d("M1 扇区8密钥认证失败");
+                if (mOnScanCardListen != null) {
+                    mOnScanCardListen.onScanCard("扇区读取失败", null);
+                }
+                return null;
+            }
+        }
+        return sb.toString();
     }
 
     private void decodeCpuCard(String cardNum) {
@@ -346,8 +317,6 @@ public class ScanCardManager {
                         String cardNum = cardType.getCardNo();
 //                        decodeM1Card(cardNum);
 
-//                        InterM1Card m1Card = CardHelper.getInstance().getM1Card();
-//                        String cardNum = m1Card.searchNo();
                         decodeM1Card2(cardNum);
 
                     } else if (cardType.getCardType() == CardReadManager.CardType.CPU_CARD.getCardType()) {
@@ -480,6 +449,107 @@ public class ScanCardManager {
                 mOnScanCardListen.onScanCard(byteArrayResult.getMsg(), null);
             }
         }
+    }
+
+
+    private String joinM1Data(String cardNum, String flipCardNum, ArrayList<String> arrayList) {
+        StringBuilder sb = new StringBuilder();
+        String key_6 = arrayList.get(0);
+        byte[] password6 = BytesHexStrUtils.hexToBytes1(key_6);
+        if (!TextUtils.isEmpty(key_6) && key_6.length() == 12) {
+            int block61 = 24;
+            byte[] data61 = CardHelper.getInstance().getM1Card().readBlock(cardNum, CardReadManager.KEY_MODE_A, block61, password6);
+            String data61String = "";
+            if (data61 != null) {
+                data61String = BytesHexStrUtils.bytesToHex(data61).replace("0", "");
+            }
+            if (data61 != null && !TextUtils.isEmpty(data61String)) {
+                byte[] data62 = CardHelper.getInstance().getM1Card().readBlock(cardNum, CardReadManager.KEY_MODE_A, block61 + 1, password6);
+                byte[] data63 = CardHelper.getInstance().getM1Card().readBlock(cardNum, CardReadManager.KEY_MODE_A, block61 + 2, password6);
+                //byte[] data63 = CardReadManager.getInstance().CardM1BlockRead((byte) 26);
+                if (data62 != null && data63 != null) {
+                    String s2 = ByteUtils.bytesToHex(data62);
+                    String s3 = ByteUtils.bytesToHex(data63);
+                    sb.append(data61String).append(s2).append(s3);
+                    AppLogUtils.d("Card M1 扇区6密钥认证成功：" + sb);
+                } else {
+                    AppLogUtils.d("Card M1 扇区6密钥认证失败2：请检查秘钥-" + key_6);
+                    if (mOnScanCardListen != null) {
+                        mOnScanCardListen.onScanCard("扇区6读取失败", null);
+                    }
+                    return null;
+                }
+            } else {
+                AppLogUtils.d("Card M1 扇区6密钥认证失败1：请检查秘钥-" + key_6);
+                if (mOnScanCardListen != null) {
+                    mOnScanCardListen.onScanCard("扇区6读取失败", null);
+                }
+                return null;
+            }
+        }
+
+        String key_7 = arrayList.get(1);
+        if (!TextUtils.isEmpty(key_7) && key_7.length() == 12) {
+            int block71 = 28;
+            byte[] data71 = readBlock(cardNum, block71, key_7);
+            String data71String = "";
+            if (data71 != null) {
+                data71String = BytesHexStrUtils.bytesToHex(data71).replace("0", "");
+            }
+            if (data71 != null && !TextUtils.isEmpty(data71String)) {
+                byte[] data72 = readBlock(cardNum, block71 + 1, key_7);
+                byte[] data73 = readBlock(cardNum, block71 + 2, key_7);
+                if (data72 != null && data73 != null) {
+                    String s2 = ByteUtils.bytesToHex(data72);
+                    String s3 = ByteUtils.bytesToHex(data73);
+                    sb.append(data71String).append(s2).append(s3);
+                    AppLogUtils.d("Card M1 扇区7密钥认证成功：" + data71String + s2 + s3);
+                } else {
+                    AppLogUtils.d("Card M1 扇区7密钥认证失败2：请检查秘钥-" + key_7);
+                    if (mOnScanCardListen != null) {
+                        mOnScanCardListen.onScanCard("扇区7读取失败", null);
+                    }
+                    return null;
+                }
+            } else {
+                AppLogUtils.d("Card M1 扇区7密钥认证失败1：请检查秘钥-" + key_7);
+                if (mOnScanCardListen != null) {
+                    mOnScanCardListen.onScanCard("扇区7读取失败", null);
+                }
+                return null;
+            }
+        }
+
+        String key_8 = arrayList.get(2);
+        if (!TextUtils.isEmpty(key_8) && key_8.length() == 12) {
+            int block81 = 32;
+            byte[] data81 = readBlock(cardNum, block81, key_8);
+            String data81String = "";
+            if (data81 != null) {
+                data81String = ByteUtils.bytesToHex(data81);
+            }
+            if (data81 != null && !TextUtils.isEmpty(data81String)) {
+                byte[] data82 = readBlock(cardNum, block81 + 1, key_8);
+                if (data82 != null) {
+                    String s2 = ByteUtils.bytesToHex(data82);
+                    sb.append(data81String).append(s2);
+                    AppLogUtils.d("Card M1 扇区8密钥认证成功：" + data81String + s2);
+                } else {
+                    AppLogUtils.d("Card M1 扇区8密钥认证失败2：请检查秘钥-" + key_8);
+                    if (mOnScanCardListen != null) {
+                        mOnScanCardListen.onScanCard("扇区8读取失败", null);
+                    }
+                    return null;
+                }
+            } else {
+                AppLogUtils.d("Card M1 扇区8密钥认证失败1：请检查秘钥-" + key_8);
+                if (mOnScanCardListen != null) {
+                    mOnScanCardListen.onScanCard("扇区8读取失败", null);
+                }
+                return null;
+            }
+        }
+        return sb.toString();
     }
 
 }
--- AppHardware/src/main/java/com/yc/apphardware/card/lib/MySerialPort.java
+++ AppHardware/src/main/java/com/yc/apphardware/card/lib/MySerialPort.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class MySerialPort{
+public class MySerialPort {
 
     private String mPort;
     private int mIBaudRate;
@@ -25,7 +25,7 @@ public class MySerialPort{
     private static byte[] cTotleLen;    //包长度
     private static byte cType;    //指令类型
     private static byte cCMD;    //指令命令  //01:打开M1卡 02:关闭M1卡 03:非接触式IC卡寻卡 04：CPU卡复位 05:CPU卡
-                                //ADPU指令11:认证M卡的密钥 12:读M卡块数据 13:读M卡块数据
+    //ADPU指令11:认证M卡的密钥 12:读M卡块数据 13:读M卡块数据
     private static byte[] cBufLen;    //指令数据长度
     private static byte cLrc;    //包LRC校验值
     private static byte cETX = 0x40;    //包尾
@@ -35,7 +35,7 @@ public class MySerialPort{
     public static long write_time;
     public static long read_time;
 
-    public MySerialPort(){
+    public MySerialPort() {
         try {
             if (mSerialPort == null) {
                 setSerialPort();
@@ -46,7 +46,7 @@ public class MySerialPort{
             e.printStackTrace();
         }
 
-        if(mSerialPort != null) {
+        if (mSerialPort != null) {
             mOutputStream = mSerialPort.getOutputStream();
             mInputStream = mSerialPort.getInputStream();
         }
@@ -54,13 +54,13 @@ public class MySerialPort{
 
     public void setSerialPort() {
         String product_model = Build.MODEL;
-        if(product_model.contains("SP306+/SP308+") || product_model.contains("SP306/SP308")) {
+        if (product_model.contains("SP306+/SP308+") || product_model.contains("SP306/SP308")) {
             mPort = "/dev/ttyMT1";
             mIBaudRate = 115200;
-        } else if(product_model.contains("SP801") || product_model.contains("FR35") || product_model.contains("SP806") || product_model.contains("S225")) {
+        } else if (product_model.contains("SP801") || product_model.contains("FR35") || product_model.contains("SP806") || product_model.contains("S225")) {
             mPort = "/dev/ttyS4";
             mIBaudRate = 115200;
-        } else if(product_model.contains("SP805")) {
+        } else if (product_model.contains("SP805")) {
             mPort = "/dev/ttyS2";
             mIBaudRate = 115200;
         } else {
@@ -80,44 +80,42 @@ public class MySerialPort{
 
         send_data[0] = cSTX;
         send_data[1] = cFrame;
-		send_data[2] = 0x00;
-		send_data[3] = 0x04;
+        send_data[2] = 0x00;
+        send_data[3] = 0x04;
         send_data[4] = type;
         send_data[5] = cmd;
-		send_data[6] = 0x00;
-		send_data[7] = 0x00;
+        send_data[6] = 0x00;
+        send_data[7] = 0x00;
         send_data[8] = MyConverterTool.CheckSum(send_data, 1, (send_data.length - 3));//计算校验码
-		send_data[9] = cETX;
+        send_data[9] = cETX;
 
         return writeData(send_data);
     }
 
     public String setCosOrder(byte[] data, byte type, byte cmd) throws Exception {
         byte[] send_data = new byte[8 + data.length + 2];  //数据组成：尾部校验码加包尾
-
         send_data[0] = cSTX;
         send_data[1] = cFrame;
-		send_data[2] = 0x00;
-		send_data[3] = (byte)(data.length + 4);
+        send_data[2] = 0x00;
+        send_data[3] = (byte) (data.length + 4);
         send_data[4] = type;
         send_data[5] = cmd;
         send_data[6] = 0x00;
-		send_data[7] = (byte)data.length;
+        send_data[7] = (byte) data.length;
 
-        for (int i = 0; i < data.length; i ++)
-        {
+        for (int i = 0; i < data.length; i++) {
             send_data[i + 8] = data[i];
         }
 
         send_data[data.length + 8] = MyConverterTool.CheckSum(send_data, 1, (send_data.length - 3));//计算校验码
-		send_data[data.length + 9] = cETX;
+        send_data[data.length + 9] = cETX;
 
         return writeData(send_data);
     }
 
     private String writeData(byte[] bOutArray) throws Exception {
         String ss = MyConverterTool.ByteArrToHex(bOutArray);
-		if(CardReadManager.getInstance().getDebugStatus()) {
+        if (CardReadManager.getInstance().getDebugStatus()) {
             Log.i("CardComm", "send command = " + ss);
             write_time = System.currentTimeMillis();
         }
@@ -133,7 +131,7 @@ public class MySerialPort{
         long start_time = System.currentTimeMillis();
         while (true) {
             //超时退出
-            if(is.available() == 0) {
+            if (is.available() == 0) {
                 if ((System.currentTimeMillis() - start_time) >= read_timeout) {
                     Log.e("CardComm", "Read Timeout!");
                     return null;
@@ -145,7 +143,7 @@ public class MySerialPort{
             StringBuilder sb = new StringBuilder();
             byte[] buffer = new byte[512];
 
-            while((is.available()) != 0) {
+            while ((is.available()) != 0) {
                 int len = is.read(buffer);
 
                 byte[] dataByte = new byte[len];
@@ -156,27 +154,26 @@ public class MySerialPort{
             }
 
             String readData = sb.toString();
-            if(CardReadManager.getInstance().getDebugStatus()) {
+            if (CardReadManager.getInstance().getDebugStatus()) {
                 Log.i("CardComm", "read data is : " + readData);
             }
 
-            if(CardReadManager.getInstance().getDebugStatus())
+            if (CardReadManager.getInstance().getDebugStatus())
                 read_time = System.currentTimeMillis() - write_time;
 
             readData = readData.replace(" ", "");
 
             byte[] readDataByte = MyConverterTool.HexToByteArr(readData);
             int readDataLength = readDataByte.length;
-            if(readDataLength < 11) {   // 有效数据长度不会少于11字节
+            if (readDataLength < 11) {   // 有效数据长度不会少于11字节
                 Log.e("CardComm", "read data length error");
                 continue;
             }
-            if(readDataByte[0] != 0x02 || readDataByte[readDataLength - 1] != 0x40) {   // 包头尾有误
+            if (readDataByte[0] != 0x02 || readDataByte[readDataLength - 1] != 0x40) {   // 包头尾有误
                 Log.e("CardComm", "data format error");
                 continue;
             }
-            if(MyConverterTool.CheckSum(readDataByte, 1, readDataLength - 3) !=
-                    readDataByte[readDataLength - 2]) {
+            if (MyConverterTool.CheckSum(readDataByte, 1, readDataLength - 3) != readDataByte[readDataLength - 2]) {
                 Log.e("CardComm", "checksum mismatch");
                 continue;
             }
@@ -185,7 +182,7 @@ public class MySerialPort{
     }
 
     //----------------------------------------------------
-    public void close(){
+    public void close() {
         if (mSerialPort != null) {
             mSerialPort.close();
             mSerialPort = null;
